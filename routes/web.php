5cfdefd80ad344b7560fc44e7e712e90d21e6230
nick130920 <91711486+jeannpier@users.noreply.github.com>
2021-10-07 09:57:32 -0500

    Revert "crud"
    This reverts commit b06677d294a76c53e07073e8315e931d8bf96c79.

--- routes/web.php
+++ routes/web.php
@@ -30,9 +30,3 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
-
-Auth::routes();
-
-route::resource('servicios',App\Http\Controllers\ServicioController:: class);
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
