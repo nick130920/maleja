0e9c9309354443852e3fc2940b9b6e0fbf9f96ca
Sagit Gutierrez <jozaguts@gmail.com>
2021-11-13 12:14:31 -0600

    background de login nueva distribucion

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,4 +1,9 @@
-{{--tratar de nunca usar inline css--}}
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 bg-login">
+{{--tratar de nunca usar inline css y desidir si usaa tailwindcss o bootstrap--}}
+<div class="min-h-screen grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+
+    <div class="flex flex-col justify-center items-center">
+        <h1 class="text-2xl font-bold my-4 ">Agregar un titulo</h1>
         {{ $slot }}
+    </div>
+    <div class="hidden md:block lg:block bg-login"></div>
 </div>
