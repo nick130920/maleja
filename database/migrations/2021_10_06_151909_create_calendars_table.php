272ae2a4038d82e703ebe4d1ecb6e5ff89f25956
nick130920 <nicolas.munoz@usco.edu.co>
2021-12-13 23:22:19 -0500

    migracion

--- database/migrations/2021_10_06_151909_create_calendars_table.php
+++ database/migrations/2021_10_06_151909_create_calendars_table.php
@@ -15,7 +15,7 @@ class CreateCalendarsTable extends Migration
     {
         Schema::create('calendars', function (Blueprint $table) {
             $table->id();
-            $table->string('start');
+            $table->dateTime('start');
             $table->string('code');
             $table->bigInteger('phone_number');
             $table->foreignId('service_id')->constrained();
