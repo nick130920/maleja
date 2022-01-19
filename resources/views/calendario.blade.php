61979d87a9fb75e8293f31616b249f696fe4788f
gabo024 <91437848+gabo024@users.noreply.github.com>
2022-01-19 21:59:23 +0100

    cambios calendario

--- resources/views/calendario.blade.php
+++ resources/views/calendario.blade.php
@@ -6,8 +6,6 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
-    {{-- <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet"> --}}
-    {{-- <script type="text/javascript" src="{{ asset('js/es-ES.js') }}"></script> --}}
     <link rel="stylesheet" href="{{ asset('css/calendar.css') }}">
 
 
@@ -65,14 +63,15 @@
                                         </tbody>
                                     </table>
                                 </div>
-                                <button class="button" id="add-button">Add Event</button>
+                                <button class="button" id="add-button">Agregar Cita</button>
                             </div>
                         </div>
                         <div class="events-container">
 
                         </div>
                         <div class="hidden">
-                            <form id="form-posponer" class="formulario_posponer" action='{{ route('calendar') }}' method='POST'>@csrf</form>
+                            <form id="form-posponer" action='{{ route('calendar') }}' method='POST'>@csrf
+                            </form>
                             <button type='button' class='mx-4 bg-info button' id='posponer'>Posponer</button>
                         </div>
 
@@ -81,16 +80,14 @@
                             <div class="form-container" align="center" style="margin-top: 0">
                                 <form action="{{ route('calendar') }}" method="POST">
                                     @csrf
-                                    <label for="title" class="form-label">Título</label>
+                                    <label for="title" class="form-label">quien solicita</label>
                                     <input type="text" required :value="old('title')" name="title" class="input" style="margin-bottom: 10px"
                                         id="title">
-                                    <label class="form-label" id="valueFromMyButton" for="count">Inicio</label>
-                                    <input type='datetime-local' min="2018-01-01" max="2200-12-31" value="2021-09-13" id="start" :value="old('start')" name="start"
-                                        class="input" style="margin-bottom: 10px" required />
+                                    <label class="form-label" id="valueFromMyButton" for="count">fecha y hora</label>
+                                    <input type='datetime-local' min="2018-01-01" max="2200-12-31" id="start" name="start" class="input" style="margin-bottom: 10px" required />
 
                                     <label for="phone_number" class="form-label">Celular</label>
-                                    <input type='number' name="phone_number" :value="old('phone_number')" id="phone_number"
-                                        class="input" style="margin-bottom: 10px" placeholder="3124567890" />
+                                    <input type='number' name="phone_number" :value="old('phone_number')" id="phone_number" class="input" style="margin-bottom: 10px" placeholder="Ejemplo: 3124567890" />
                                     <label for="class" class="form-label">Tipo servicio</label>
                                     <select style="background: #000;" class="input-select" aria-label="Default select example" name="service_id"
                                         :value="old('service_id')" require>
@@ -99,19 +96,6 @@
                                             <option style="color: #999" value={{ $servicio->id }}>{{ $servicio->name }}
                                                 {{ $servicio->time }}</option>
                                         @endforeach
-                                        <option style="color: #999" value="3">corte de caballeros</option>
-                                        <option style="color: #999" value="4">pedicure</option>
-                                        <option style="color: #999" value="5">manicure</option>
-                                        <option style="color: #999" value="6">Depilacion con cera </option>
-                                        <option style="color: #999" value="7">Maquillaje</option>
-                                        <option style="color: #999" value="8">Trenza</option>
-                                        <option style="color: #999" value="9">peinado niñas </option>
-                                        <option style="color: #999" value="10">peinado damas </option>
-                                        <option style="color: #999" value="11">planchados</option>
-                                        <option style="color: #999" value="12">cepillado</option>
-                                        <option style="color: #999" value="13">Keratina </option>
-                                        <option style="color: #999" value="14">Tintes</option>
-                                        <option style="color: #999" value="15">semipermanentes</option>
                                     </select>
                                     <br>
                                     <label for="body" class="form-label">Evento</label>
@@ -126,58 +110,11 @@
                 </div>
             </div>
     </section>
-    <!--Section Calendario-->
-    {{-- <div class="container"> --}}
-    {{-- <div class="row">
-            <!--<div class="page-header"><h4></h4></div>-->
-            <div class="float-xs-start form-inline"><br>
-                <div class="btn-group">
-                    <button class="btn btn-primary" data-calendar-nav="prev"><i class="fa fa-arrow-left"></i>
-                    </button>
-                    <button class="btn" data-calendar-nav="today">Hoy</button>
-                    <button class="btn btn-primary" data-calendar-nav="next"><i class="fa fa-arrow-right"></i>
-                    </button>
-                </div>
-                <div class="btn-group">
-                    <button class="btn btn-warning" data-calendar-view="year">Año</button>
-                    <button class="btn btn-warning active" data-calendar-view="month">Mes</button>
-                    <button class="btn btn-warning" data-calendar-view="week">Semana</button>
-                    <button class="btn btn-warning" data-calendar-view="day">Dia</button>
-                </div>
-            </div>
-            <div class="float-xs-end form-inline"><br>
-                <button class="btn btn-info" data-bs-toggle='modal' data-bs-target='#add_evento'>Añadir Evento</button>
-            </div>
-        </div>
-        <br><br><br> --}}
-    {{-- <div class="row">
-            <div id="calendar"></div> <!-- Aqui se mostrara nuestro calendario -->
-
-        </div> --}}
-    <!--ventana modal para el calendario-->
-    {{-- <div class="modal fade" id="events-modal">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <a href="#" data-bs-dismiss="modal" style="float: right;"> <i
-                                class="glyphicon glyphicon-remove "></i>
-                        </a>
-                        <br>
-                    </div>
-                    <div class="modal-body" style="height: 400px">
-                        <p>One fine body&hellip;</p>
-                    </div>
-
-                </div><!-- /.modal-content -->
-            </div><!-- /.modal-dialog -->
-        </div><!-- /.modal --> --}}
-    {{-- </div> --}}
+    
     <script src="{{ asset('js/underscore-min.js') }}"></script>
     <script src="{{ asset('js/calendar.js') }}"></script>
     <script src="{{ asset('js/jquery.min.js') }}"></script>
     <script src="{{ asset('js/moment.js') }}"></script>
-    {{-- <script src="{{ asset('js/bootstrap-datetimepicker.js') }}"></script> --}}
-    {{-- <link rel="stylesheet" href="{{ asset('css/bootstrap-datetimepicker.min.css') }}" /> --}}
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
     <style>
         #start::-webkit-calendar-picker-indicator {
@@ -195,181 +132,4 @@
             position: relative;
             right: 20px;
         }</style>
-    {{-- <script type="text/javascript">
-        (function($){
-                //creamos la fecha actual
-                var date = new Date();
-                var yyyy = date.getFullYear().toString();
-                var mm = (date.getMonth()+1).toString().length == 1 ? "0"+(date.getMonth()+1).toString() : (date.getMonth()+1).toString();
-                var dd  = (date.getDate()).toString().length == 1 ? "0"+(date.getDate()).toString() : (date.getDate()).toString();
-
-                var citas = @json($citas);
-                //establecemos los valores del calendario
-                var options = {
-
-                    // definimos que los agenda se mostraran en ventana modal
-                    modal: '#events-modal',
-
-                        // dentro de un iframe
-                        modal_type:'iframe',
-
-                        //obtenemos los agenda de la base de datos
-                        events_source: 'obtener_eventos.php',
-
-                        // mostramos el calendario en el mes
-                        view: 'month',
-
-                        // y dia actual
-                        day: yyyy+"-"+mm+"-"+dd,
-
-
-                        // definimos el idioma por defecto
-                        language: 'es-ES',
-
-                        //Template de nuestro calendario
-                        tmpl_path: 'tmpls/',
-                        tmpl_cache: false,
-
-
-                        // Hora de inicio
-                        time_start: '08:00',
-
-                        // y Hora final de cada dia
-                        time_end: '22:00',
-
-                        // intervalo de tiempo entre las hora, en este caso son 30 minutos
-                        time_split: '30',
-
-                        // Definimos un ancho del 100% a nuestro calendario
-                        width: '100%',
-
-                        onAfterEventsLoad: function(events)
-                        {
-                            if(!events)
-                            {
-                                return;
-                            }
-                            var list = $('#eventlist');
-                            list.html('');
-
-                            $.each(events, function(key, val)
-                            {
-                                $(document.createElement('li'))
-                                .html('<a href="' + val.url + '">' + val.title + '</a>')
-                                .appendTo(list);
-                            });
-                        },
-                        onAfterViewLoad: function(view)
-                        {
-                            $('#page-header').text(this.getTitle());
-                            $('.btn-group button').removeClass('active');
-                            $('button[data-calendar-view="' + view + '"]').addClass('active');
-                        },
-                        classes: {
-                            months: {
-                                general: 'label'
-                            }
-                        }
-                    };
-
-
-                // id del div donde se mostrara el calendario
-                var calendar = $('#calendar').calendar(options);
-
-                $('.btn-group button[data-calendar-nav]').each(function()
-                {
-                    var $this = $(this);
-                    $this.click(function()
-                    {
-                        calendar.navigate($this.data('calendar-nav'));
-                    });
-                });
-
-                $('.btn-group button[data-calendar-view]').each(function()
-                {
-                    var $this = $(this);
-                    $this.click(function()
-                    {
-                        calendar.view($this.data('calendar-view'));
-                    });
-                });
-
-                $('#first_day').change(function()
-                {
-                    var value = $(this).val();
-                    value = value.length ? parseInt(value) : null;
-                    calendar.setOptions({first_day: value});
-                    calendar.view();
-                });
-            }(jQuery));
-    </script> --}}
-    {{-- <div class="modal fade" id="add_evento" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
-        aria-hidden="false">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title" id="myModalLabel">Agregar nuevo evento</h4>
-                </div>
-                <div class="modal-body">
-                    <form action="{{ route('calendar') }}" method="POST">
-                        @csrf
-                        <label for="start" class="form-label">Inicio</label>
-                        <div class='input-group date' id='start'>
-                            <input type='text' id="start" :value="old('start')" name="start" class="form-control" aria-label="Recipient" aria-describedby="basicoo" required />
-                            <span id="basic-addon2" class="input-group-addon"><span  class="glyphicon glyphicon-calendar"></span>
-                            <span class="input-group-text" id="basicoo">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar-week-fill"
-                                    viewBox="0 0 16 16">
-                                    <path
-                                        d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4V.5zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2zM9.5 7h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm3 0h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zM2 10.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm3.5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5z">
-                                    </path>
-                                </svg>
-                            </span>
-                        </div>
-                        <br>
-                        <label for="phone_number" class="form-label">Celular</label>
-                        <input type='number' name="phone_number" :value="old('phone_number')" id="phone_number" class="form-control"
-                            placeholder="3124567890" />
-                        <br>
-                        <label for="class" class="form-label">Tipo servicio</label>
-                        <select class="form-select" aria-label="Default select example" name="service_id" :value="old('service_id')" required>
-                            <option></option>
-                            @foreach ($servicios as $servicio)
-                                <option value={{ $servicio->id  }}>{{ $servicio->name  }} {{$servicio->time}}</option>
-                            @endforeach
-                            <option value="1">corte de dama</option>
-                            <option value="2">corte de niños</option>
-                            <option value="3">corte de caballeros</option>
-                            <option value="4">pedicure</option>
-                            <option value="5">manicure</option>
-                            <option value="6">Depilacion con cera </option>
-                            <option value="7">Maquillaje</option>
-                            <option value="8">Trenza</option>
-                            <option value="9">peinado niñas </option>
-                            <option value="10">peinado damas </option>
-                            <option value="11">planchados</option>
-                            <option value="12">cepillado</option>
-                            <option value="13">Keratina </option>
-                            <option value="14">Tintes</option>
-                            <option value="15">semipermanentes</option>
-                        </select>
-                        <br>
-                        <label for="title" class="form-label">Título</label>
-                        <input type="text" required :value="old('title')" name="title" class="form-control" id="title"
-                            placeholder="Introduce un título">
-                        <br>
-                        <label for="body" class="form-label">Evento</label>
-                        <textarea id="body" name="event" :value="old('event')" required class="form-control" rows="3"></textarea>
-
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal"><i class="fa fa-times"></i>
-                        Cancelar</button>
-                    <button type="submit" class="btn btn-success"><i class="fa fa-check"></i> Agregar</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div> --}}
-    <!--Section Calendario-->
 @endsection
--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -3,25 +3,28 @@
       <div class="container">
         <div class="heading">
           <h2>Contactos</h2>
-          <h3>contactanos</h3>
         </div>
       </div>
 
       <div class="container">
         <div class="row">
-          <div class="col-md-7">
-            <div class="map">
+          <div class="col-7">
+            <div class="map shadow p-3 mb-5 bg-body rounded">
               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3985.426902604837!2d-75.32592835021585!3d2.688458198031736!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3b6aa74ce7de8d%3A0xc1a4e03611799ca3!2sCra.%209%20%2323-100%2C%20Campo%20Alegre%2C%20Campoalegre%2C%20Huila!5e0!3m2!1ses!2sco!4v1631026001017!5m2!1ses!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
             </div>
           </div>
 
-          <div class="contact-info">
-            <div class="col-md-5">
+          <div class="col-5 mt-5">
+            <div class="card-header p-4" style="background: #99F939;">
+          </div>
+              
+              <div class="card-body shadow p-4 mb-5 bg-body rounded">
+            <div class="contact-info">
 
               <ul>
-                <li><i class="fa fa-home fa-2x"></i> Cra.9 #23-100 campoalegre (Huila)</li>
-                <li><i class="fa fa-phone fa-2x"></i> +573133329614</li>
-                <li><i class="fa fa-envelope fa-2x"></i>Maleja.oficial07@gmail.com </li>
+                <li><i class="fa fa-home fa-2x"></i>  Cra.9 #23-100 campoalegre (Huila)</li>
+                <li><i class="fa fa-phone fa-2x"></i>  +573133329614</li>
+                <li><i class="fa fa-envelope fa-2x"></i>  Maleja.oficial07@gmail.com </li>
 
               </ul>
             </div>
@@ -30,8 +33,6 @@
       </div>
 
 
-            </form>
-          </div>
         </div>
       </div>
 
